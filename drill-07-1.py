3594b9fa67c1af8c6ae01e3fb7cc0987455e443a
baejisoo <fanatic0717@naver.com>
2018-10-04 16:34:41 +0900

    2015182043
    p2 - p3 이동 구현

--- drill-07-1.py
+++ drill-07-1.py
@@ -71,6 +71,17 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p2)
 
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+        draw_point((x, y))
+    draw_point(p3)
+
+
 
 
 prepare_turtle_canvas()
